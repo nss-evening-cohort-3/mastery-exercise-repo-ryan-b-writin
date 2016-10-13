858342eb5189fac08319d3b45e2179e9a8dcad61
Jurnell Cockhren <jurnell@sophicware.com>
2016-10-12 19:23:00 -0700

    Added notes

--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -7,5 +7,8 @@ namespace RepoQuiz.DAL
 {
     public class NameGenerator
     {
+        // This class should be used to generate random names and Majors for Students.
+        // This is NOT your Repository
+        // All methods should be Unit Tested :)
     }
 }
