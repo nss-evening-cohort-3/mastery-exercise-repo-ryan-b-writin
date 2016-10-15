6501ab7297317a002590b4c4e64143f356906554
Ryan B <admirallasers@gmail.com>
2016-10-15 11:49:49 -0500

    set up mocks in unit tests

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,50 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoQuiz.DAL;
+using Moq;
+using RepoQuiz.Models;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
-    {
+    { 
+        Mock<StudentContext> mock_context { get; set; }
+        Mock<DbSet<Student>> mock_student_table { get; set; }
+        List<Student> studentList { get; set; }
+        StudentRepository repo { get; set; }
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = studentList.AsQueryable();
+
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            mock_context.Setup(c => c.Students).Returns(mock_student_table.Object);
+
+            mock_student_table.Setup(t => t.Add(It.IsAny<Student>())).Callback((Student a) => studentList.Add(a));
+            mock_student_table.Setup(t => t.Remove(It.IsAny<Student>())).Callback((Student a) => studentList.Remove(a));
+            mock_student_table.Setup(t => t.RemoveRange(It.IsAny<IEnumerable<Student>>())).Callback((IEnumerable<Student> a) => studentList.RemoveRange(0, a.Count<Student>()));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<StudentContext>();
+            mock_student_table = new Mock<DbSet<Student>>();
+            studentList = new List<Student>();
+            repo = new StudentRepository(mock_context.Object);
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+            repo = null;
+        }
     }
 }
